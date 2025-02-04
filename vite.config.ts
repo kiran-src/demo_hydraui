52af9b5d842b9ae9bd66220c42483db45ef8710b
Kiran Setty <68366418+kiran-src@users.noreply.github.com>
2025-02-04 12:03:22 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from '@angular-devkit/build-angular/src/tools/turbo';
+// import { defineConfig } from '@angular-devkit/build-angular/src/tools/turbo';
 
-export default defineConfig({
-  optimizeDeps: {
-    include: ['@stomp/stompjs', 'sockjs-client'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      'global': 'globalThis',
-      'process': 'process/browser',
-      'buffer': 'buffer',
-      'util': 'util'
-    }
-  },
-  define: {
-    'global': 'globalThis',
-    'process.env': {},
-    'process.version': '"v16.0.0"'
-  }
-});
+// export default defineConfig({
+//   optimizeDeps: {
+//     include: ['@stomp/stompjs', 'sockjs-client'],
+//     esbuildOptions: {
+//       define: {
+//         global: 'globalThis'
+//       }
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       'global': 'globalThis',
+//       'process': 'process/browser',
+//       'buffer': 'buffer',
+//       'util': 'util'
+//     }
+//   },
+//   define: {
+//     'global': 'globalThis',
+//     'process.env': {},
+//     'process.version': '"v16.0.0"'
+//   }
+// });
